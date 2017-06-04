68da53ceba2805d8e585f344928a948f0068f589
zayen.chagra <zayen.chagra@gmail.com>
2017-06-04 10:06:37 +0200

    first commit

---
+++ StringHelper/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace StringHelper
+{
+	public class MyStringHelper
+	{
+		public static String MettreEnMajusculePrenom(String surnameToCrop)
+		{
+			String[] surname = surnameToCrop.Split('-');
+			StringBuilder stringBuilder = new StringBuilder();
+
+			for (Int32 i = 0; i < surname.Length; i++)
+			{
+				stringBuilder.Append(surname[i].Substring(0, 1).ToUpper() + surname[i].Substring(1));
+		
+				if (i < surname.Length - 1)
+					stringBuilder.Append("-");
+			}
+
+			return stringBuilder.ToString();   
+			                              
+	}
+}
